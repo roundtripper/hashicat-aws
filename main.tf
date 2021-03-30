1aed5d1a5b6c1de5e4f0e8315905b7bb0a5db0fd
Rich Tasi <roundtripper@snet.net>
2021-03-30 18:15:18 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -116,8 +116,6 @@ resource "aws_eip_association" "hashicat" {
 }
 
 resource "aws_instance" "hashicat" {
-  billable                    = true
-  department                  = devops
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.hashicat.key_name
